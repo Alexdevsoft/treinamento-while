22311a975a8ed2b09ae285d5cd215c2e50e1cf47
Alexdevsoft <alexhavilla2022@gmail.com>
2025-01-01 22:27:56 -0300

    While commitado

--- src/treinamento/Main.java
+++ src/treinamento/Main.java
@@ -3,7 +3,14 @@ package treinamento;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 5;
+		
+		
+		while(a < 10) {
+			a += 1;
+		}
+		System.out.println(a);
 
 	}
 
