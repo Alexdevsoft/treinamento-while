7c2f23ffc0335cd97b6c52d9ff85f5873b4be8ca
Alexdevsoft <alexhavilla2022@gmail.com>
2025-01-02 22:55:37 -0300

    a e b

--- src/treinamento/Main.java
+++ src/treinamento/Main.java
@@ -5,12 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int a = 5;
-		
+		int b = 0;
 		
 		while(a != 10) {
 			a += 1;
+			b = a + 20;
 		}
-		System.out.println(a);
+		System.out.println(b);
 
 	}
 
